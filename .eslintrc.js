2580630f4b97624b938e56628f4c9461932f03ff
Federico <fededevsit@gmail.com>
2022-07-27 22:41:41 -0300

    ts config

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@next/next/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['./tsconfig.json'],
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'eslint-plugin-prettier'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'require-jsdoc': 'off',
+        'react/prop-types': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+    },
+};
